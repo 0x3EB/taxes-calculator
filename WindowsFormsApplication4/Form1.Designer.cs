2f3e29ee4830ee946a4aa312445d3a920a648450
Sébastien M.L <sebastien.lejeune6@gmail.com>
2019-01-30 14:51:27 +0100

    1st commit

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculImpotB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.netTextBox = new System.Windows.Forms.TextBox();
+            this.PartsCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // CalculImpotB
+            // 
+            this.CalculImpotB.Location = new System.Drawing.Point(73, 156);
+            this.CalculImpotB.Name = "CalculImpotB";
+            this.CalculImpotB.Size = new System.Drawing.Size(124, 41);
+            this.CalculImpotB.TabIndex = 0;
+            this.CalculImpotB.Text = "Calcul des impots";
+            this.CalculImpotB.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre de part :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Saisir votre revenu :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // netTextBox
+            // 
+            this.netTextBox.Location = new System.Drawing.Point(22, 87);
+            this.netTextBox.Name = "netTextBox";
+            this.netTextBox.Size = new System.Drawing.Size(100, 20);
+            this.netTextBox.TabIndex = 4;
+            // 
+            // PartsCombo
+            // 
+            this.PartsCombo.FormattingEnabled = true;
+            this.PartsCombo.Location = new System.Drawing.Point(151, 87);
+            this.PartsCombo.Name = "PartsCombo";
+            this.PartsCombo.Size = new System.Drawing.Size(90, 21);
+            this.PartsCombo.TabIndex = 5;
+            this.PartsCombo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(277, 254);
+            this.Controls.Add(this.PartsCombo);
+            this.Controls.Add(this.CalculImpotB);
+            this.Controls.Add(this.netTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculImpotB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox netTextBox;
+        private System.Windows.Forms.ComboBox PartsCombo;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 15; i++)
+            {
+                PartsCombo.Items.Add(i+1);
+            }
+            if (string.IsNullOrEmpty(netTextBox.Text) && string.IsNullOrWhiteSpace(PartsCombo.Text))
+            {
+                CalculImpotB.Visible = true;
+            }
+            else
+                CalculImpotB.Visible = true;
+
+            CalculImpotB.Click += CalculImpotB_Click;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculImpotB_Click(object sender, EventArgs e)
+        {
+            
+            int net, parts, impots, reste;
+            string StrImpots;
+            net = Convert.ToInt32(netTextBox.Text);
+            net = int.Parse(netTextBox.Text);
+            parts = Convert.ToInt32(PartsCombo.SelectedIndex+1);
+
+            if (net < 9710)
+                impots = 0;
+            else if (net < 26818)
+            {
+                reste = net - 9710;
+                impots = (reste * 14) / 100;
+            }
+            else if (net < 71898)
+            {
+                reste = net - 26818;
+                impots = (17108 * 14) / 100 + (reste * 30) / 100;
+            }
+            else if (net < 152260)
+            {
+                reste = net - 71898;
+                impots = (17108 * 14) / 100 + (45080 * 30) / 100 + (reste * 41) / 100;
+            }
+            else
+            {
+                reste = net - 152260;
+                impots = (17108 * 14) / 100 + (45080 * 30) / 100 + (80362 * 41) / 100 + (reste * 45) / 100;
+            }
+
+            impots = impots * parts;
+            net = net * parts;
+
+            StrImpots = impots.ToString();
+            MessageBox.Show(StrImpots);
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			/*
+            Timer timer = new Timer();
+            timer.Interval = (10 * 1000); // 10 secs
+            timer.Tick += new EventHandler();
+            timer.Start();
+            */
+        }
+
+
+    }
+}
